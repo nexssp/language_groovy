592d13501f9eb55eaa021303d790ef4530cbaaf1
Marcin Polak <mapoart@gmail.com>
2020-09-03 12:30:51 +0200

    Added Linux and MacOS

--- groovy.linux.nexss.config.js
+++ groovy.linux.nexss.config.js
@@ -1,7 +1,7 @@
 let languageConfig = Object.assign({}, require("./groovy.win32.nexss.config"));
 languageConfig.compilers = {
   groovy: {
-    install: `bash -c "apt update && apt install openjdk-8-jdk openjdk-8-doc && apt install curl && curl -s get.sdkman.io | bash && source \"$HOME/.sdkman/bin/sdkman-init.sh\" && sdk install groovy"`,
+    install: `bash -c "apt update && apt install openjdk-8-jdk openjdk-8-doc && snap install groovy --classic"`,
     command: "groovy",
     args: "<file>",
     help: ``,
