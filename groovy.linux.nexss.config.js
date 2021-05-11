490d654fb3134dc477726f77a16eda6e3c45ed40
Marcin Polak <mapoart@gmail.com>
2021-05-12 01:26:36 +0200

    Changed install to apt-get install groovy

--- groovy.linux.nexss.config.js
+++ groovy.linux.nexss.config.js
@@ -4,16 +4,10 @@ const sudo = process.sudo;
 languageConfig.compilers = {
   groovy: {
     // install: `bash -c "apt update && apt install openjdk-8-jdk openjdk-8-doc && snap install groovy --classic"`,
-    install: `apt install unzip zip
-echo "export SDKMAN_DIR=\"$HOME/.sdkman\"" > ~/.bashrc
-export PATH="$HOME/.sdkman:$PATH"    
-. ~/.bashrc
-${sudo}curl -s get.sdkman.io | bash
-chmod +x "$HOME/.sdkman/bin/sdkman-init.sh" &2>null
-"$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install groovy`,
+    install: `apt-get update && apt-get install groovy`,
     command: "groovy",
-    args: "--illegal-access=none <file>",
+    // args: "--illegal-access=none <file>",
+    args: "<file>",
     help: ``,
   },
 };
@@ -23,16 +17,16 @@ languageConfig.dist = distName;
 
 // TODO: Later to cleanup this config file !!
 switch (distName) {
-  case "Arch Linux":
-    languageConfig.compilers.groovy.install = `apt install unzip zip
-echo "export SDKMAN_DIR=\"$HOME/.sdkman\"" > ~/.bashrc
-export PATH=":$PATH"
-. ~/.bashrc
-${sudo}curl -s get.sdkman.io | bash
-chmod +x "$HOME/.sdkman/bin/sdkman-init.sh" &2>null
-"$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install groovy`;
-    break;
+  //   case "Arch Linux":
+  //     languageConfig.compilers.groovy.install = `apt install unzip zip
+  // echo "export SDKMAN_DIR=\"$HOME/.sdkman\"" > ~/.bashrc
+  // export PATH=":$PATH"
+  // . ~/.bashrc
+  // ${sudo}curl -s get.sdkman.io | bash
+  // chmod +x "$HOME/.sdkman/bin/sdkman-init.sh" &2>null
+  // "$HOME/.sdkman/bin/sdkman-init.sh"
+  // sdk install groovy`;
+  //     break;
   default:
     languageConfig.compilers.groovy.install = process.replacePMByDistro(
       languageConfig.compilers.groovy.install
